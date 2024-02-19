619f20461a43bc559d87c4de48829a4545f70ecf
zabbix-byte <zabbix@ztrunk.space>
2024-02-19 21:32:12 +0100

    [major] Basic interaction with li_at linkedin.

--- Chrome/script.js
+++ Chrome/script.js
@@ -1,7 +1,7 @@
 var li_at = ''
 var host = 'http://localhost/'
 
-function getCookies(domain, name, callback) {
+async function getCookies(domain, name, callback) {
     chrome.cookies.get({ "url": domain, "name": name }, function (cookie) {
         if (callback) {
             callback(cookie.value);
@@ -9,12 +9,14 @@ function getCookies(domain, name, callback) {
     });
 }
 
+getCookies("https://www.linkedin.com/", "li_at", function (value) {
+    li_at = value
+});
+
 document.getElementById("connect").addEventListener("click", connect);
 
 function connect() {
-    getCookies("https://www.linkedin.com/", "li_at", function (value) {
-        li_at = value
-    });
+
     var link = host + 'update_linkedin_access?li_at=' + li_at;
     chrome.tabs.create({ url: link });
 }
