5841515df2067a2139aafe4a83f359fcf68bb4e4
zabbix-byte <zabbix@ztrunk.space>
2024-02-20 17:50:29 +0100

    [minor] Some logic changes.

--- Chrome/script.js
+++ Chrome/script.js
@@ -15,8 +15,16 @@ getCookies("https://www.linkedin.com/", "li_at", function (value) {
 
 document.getElementById("connect").addEventListener("click", connect);
 
-function connect() {
 
+function openLink(link) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var tab = tabs[0];
+        chrome.tabs.update(tab.id, { url: link });
+    });
+}
+
+
+function connect() {
     var link = host + 'update_linkedin_access?li_at=' + li_at;
-    chrome.tabs.create({ url: link });
+    openLink(link);
 }
